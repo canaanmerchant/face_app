65f27c66e8982609a50a8d0b16e3208f1195f6be
cmerchant <@cmerchant>
2019-07-19 17:08:19 -0400

    reworking File Structure

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Rank = () => {
+  return (
+    <div>
+      <div className='white f3'>
+        {'Canaan, your rank is ...'}
+      </div>
+      <div className='white f4'>
+        {'#11'}
+      </div>
+    </div>
+  );
+}
+
+export default Rank;
