fecfb4bfb4296849a1821638e218298258061201
cmerchant <@cmerchant>
2019-07-31 16:18:49 -0400

    front end finished

--- src/App.js
+++ src/App.js
@@ -37,9 +37,27 @@ class App extends Component {
       box: {},
       route: 'SignIn',
       isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: '',
+
+      }
       }
     }
 
+    loadUser = (data) => {
+      this.setState({user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+
+      }})
+    }
     calculateFaceLocation = (data) => {
       const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
       const image = document.getElementById('inputImage');
@@ -62,12 +80,56 @@ class App extends Component {
       this.setState({input: event.target.value});
     }
 
-    onButtonSubmit = () => {
-      this.setState( {imageUrl: this.state.input})
-      app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-      .catch(err => console.log(err));
+  /* Where is the missing curly brace????
+
+  onButtonSubmit = () => {
+      this.setState({imageUrl: this.state.input});
+      app.models
+        .predict(
+          Clarifai.FACE_DETECT_MODEL,
+          this.state.input)
+        .then(response => {
+          if (response) {
+            fetch('http://localhost:3000/image'), {
+              method: 'put',
+              headers: {'Content-Type': 'application/json'},
+              body: JSON.stringify({
+                id: this.state.user.id
+              })
+            })
+          }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+        .catch(err => console.log(err));
+      } */
+
+
+
+   onButtonSubmit = () => {
+          this.setState({imageUrl: this.state.input});
+          app.models
+            .predict(
+              Clarifai.FACE_DETECT_MODEL,
+              this.state.input)
+            .then(response => {
+              if (response) {
+                fetch('http://localhost:3000/image', {
+                  method: 'put',
+                  headers: {'Content-Type': 'application/json'},
+                  body: JSON.stringify({
+                    id: this.state.user.id
+                  })
+                })
+                .then(response => response.json())
+                .then(count => {
+                  this.setState(Object.assign(this.state.user, {entries: count}))
+                })
+            }
+              this.displayFaceBox(this.calculateFaceLocation(response))
+            })
+            .catch(err => console.log(err));
         }
+
 
     onRouteChange = (route) => {
       if (route === 'signout') {
@@ -91,7 +153,9 @@ render() {
 
         ? <div>
             <Logo />
-            <Rank />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}/>
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit} />
@@ -100,9 +164,13 @@ render() {
               imageUrl={imageUrl}/>
             </div>
               : (
-                    route === 'SignIn'
-                  ?  <SignIn onRouteChange={this.onRouteChange}/>
-                  :  <Register onRouteChange={this.onRouteChange}/>
+                    route === 'signin'
+                  ?  <SignIn
+                        loadUser={this.loadUser}
+                        onRouteChange={this.onRouteChange}/>
+                  :  <Register
+                        loadUser={this.loadUser}
+                        onRouteChange={this.onRouteChange}/>
               )
             }
         </div>
